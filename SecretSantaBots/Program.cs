1d8fbe9499cd18ee902b4cdbf20889fad24fff06
artem-mutaf <artik.mutaf@gmail.com>
2024-12-23 12:21:59 +0200

    program

--- SecretSantaBots/Program.cs
+++ SecretSantaBots/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SecretSantaBots.DataBase;
+using SecretSantaBots.Services;
+
+
 Console.WriteLine("Hello, World!");
+var host = ApplicationDbContext.CreateHostBuilder(args).Build();
+
+
+
+
+
